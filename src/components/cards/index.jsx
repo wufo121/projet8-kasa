e3833fa123b8c77dc5241a1b05c8fd2e2436460f
wufo121 <wufo121@gmail.com>
2023-08-04 20:30:56 +0200

    test

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function Card({ id, title, cover }) {
   return (
-    <Link to={`/Logements/${id}`} className="card-link">
+    <Link to={`/Logements/${id}`} className="card-link" >
       <div className="card">
         <img src={cover} alt={title} />
         <h3>{title}</h3>
@@ -13,6 +13,7 @@ function Card({ id, title, cover }) {
     </Link>
   );
 }
+
 
 function Cards() {
   return (

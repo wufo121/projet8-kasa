0b87ec3d31dab113926cefe94d117afcb080004e
wufo121 <wufo121@gmail.com>
2023-08-18 11:20:47 +0200

    .

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you use React Router for routing
+import { Link } from 'react-router-dom'; 
 import logo from '../../assets/logo-Kasa.png';
 import '../../style/Header.scss';
 
